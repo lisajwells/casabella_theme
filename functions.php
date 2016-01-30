38d45356be4eee827876f0938de887a33fa347e3
lisajwells <lisawells999@gmail.com>
2016-01-30 13:52:44 -0500

    going to pull from fw

--- functions.php
+++ functions.php
@@ -281,3 +281,11 @@ function casa_image_banner() {
         echo '</div>';
 	}
 }
+
+
+//* Customize the entry meta in the entry header (requires HTML5 theme support)
+add_filter( 'genesis_post_info', 'sp_post_info_filter' );
+function sp_post_info_filter($post_info) {
+	$post_info = '[post_date] by [post_author_posts_link]';
+	return $post_info;
+}
